cc66f28c16506437eab5eef44dff68c4de65bcb5
Lucas M. Bacelo <bacelolucas@gmail.com>
2019-04-26 21:28:50 -0300

    Add casebutton.py script
    Script needed for power on/off button in Raspberry Pi

---
+++ Python/casebutton.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+#Import the modules to send commands to the system and access GPIO pins
+import RPi.GPIO as GPIO
+import os
+import time
+
+#Set pin numbering to board numbering
+GPIO.setmode(GPIO.BOARD)
+
+#Power led
+GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH)
+
+#Button
+GPIO.setup(5, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+while True:
+	#Set up an interrupt to look for pressed button
+	GPIO.wait_for_edge(5, GPIO.FALLING) 
+	time.sleep(1)
+
+	#Antidebounce
+	if GPIO.input(5) == False:		
+		GPIO.output(13, False)
+		time.sleep(0.1)
+		GPIO.output(13, True)
+		time.sleep(0.1)
+		GPIO.output(13, False)
+		time.sleep(0.1)
+		GPIO.output(13, True)
+		time.sleep(0.1)
+		GPIO.output(13, False)
+		time.sleep(0.1)
+		GPIO.output(13, True)
+		time.sleep(0.1)
+		#Shutdown
+		os.system('shutdown -h now')
